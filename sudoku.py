c8eb4f540da1989551bcf05e3ccf346ac6338db4
Matthew Hopkins <matthew.hopkins9942@gmail.com>
2023-04-26 09:39:00 +0100

    Added house masks

--- sudoku.py
+++ sudoku.py
@@ -14,6 +14,19 @@ import numpy as np
 
 
 class Puzzle:
+    
+    # house masks - take & of a valMask with another mask
+    valMask = [np.tile((np.arange(9)==m).reshape(9,1,1), (1,9,9)) for m in range(9)]
+    rowMask = [np.tile((np.arange(9)==m).reshape(1,9,1), (9,1,9)) for m in range(9)]
+    colMask = [np.tile((np.arange(9)==m).reshape(1,1,9), (9,9,1)) for m in range(9)]
+    sqrMask = [np.tile(
+               np.kron((np.arange(9)==m).reshape(1,3,3), np.ones((3,3))).astype(bool),
+               (9,1,1)) for m in range(9)]
+    #For sqrMask, uses Kronecker product to expand a 3*3 array with one True
+    # to a 9*9 with the sqr pattern, then tiles into 9*9*9
+    def sqr(i,j):
+        return np.ravel_multi_index((i//3, j//3), (3,3))
+    
     def __init__(self, board=None, preset='medium1'):
         """Could also add presets for testing"""
         if board is not None:
@@ -40,7 +53,6 @@ class Puzzle:
             board[i,:] = [int(n) for n in input(f"Enter {('1st' if i==0 else ('2nd' if i==1 else ('3rd' if i==2 else f'{i+1}th')))} line: ")]
         return cls(board)
     
-    
     def initPoss(self):
         """
         Calculate possibilities just based on solved cells,
@@ -53,10 +65,10 @@ class Puzzle:
             for j in range(9):
                 if self.board[i,j] != 0:
                     k = self.board[i,j]-1
-                    poss[:,i,j] = np.full(9, False)
-                    poss[k,:,j] = np.full(9, False)
-                    poss[k,i,:] = np.full(9, False)
-                    poss[k, (i//3)*3:(i//3)*3+3, (j//3)*3:(j//3)*3+3] = np.full((3,3), False)
+                    poss[:,i,j] = False
+                    poss[k,:,j] = False
+                    poss[k,i,:] = False
+                    poss[k, (i//3)*3:(i//3)*3+3, (j//3)*3:(j//3)*3+3] = False
                     poss[k,i,j] = True
         # This is copied from solveCell, solveCell not used because that 
         # requires self.poss to already be defined
@@ -72,19 +84,26 @@ class Puzzle:
         or, when cell ij is determined to be True
         Order of indices is kij as k determines depth (first layer of nesting)
         i is row (2nd layer) and j is column (3rd layer)
+        
+        Could use house masks, but this is possibly less clear
+        self.poss[rowMask[i]&colMask[j]] = False
+        self.poss[valMask[k]&rowMask[i]] = False
+        self.poss[valMask[k]&colMask[j]] = False
+        self.poss[valMask[k]&sqrMask[sqr(i,j)]] = False
+        
         """
+        
         self.board[i,j]=k+1
         
-        self.poss[:,i,j] = np.full(9, False)
-        self.poss[k,:,j] = np.full(9, False)
-        self.poss[k,i,:] = np.full(9, False)
-        self.poss[k, (i//3)*3:(i//3)*3+3, (j//3)*3:(j//3)*3+3] = np.full((3,3), False)
+        self.poss[:,i,j] = False
+        self.poss[k,:,j] = False
+        self.poss[k,i,:] = False
+        self.poss[k, (i//3)*3:(i//3)*3+3, (j//3)*3:(j//3)*3+3] = False
         
         self.poss[k,i,j] = True
         return 0
     
-    #Calculation of houseMasks
-    # houseMasks = ([np.full((9,9,9),))]+[]+[]+[]
+    
         
     def solve(self):
         """Main method, solves puzzle.
